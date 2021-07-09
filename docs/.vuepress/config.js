49caca7800355de61e5b94b824b5ab903fe8f848
IlhamTahir <yantaq@bilig.biz>
2021-07-10 01:05:01 +0800

    add catelog

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,8 +1,18 @@
 module.exports = {
-    base: '/mp-course-for-uyghur-language-starter/',
-    theme: 'default-uyghur',
+	base: '/mp-course-for-uyghur-language-starter/',
+	theme: 'default-uyghur',
 	themeConfig: {
 		lastUpdated: 'يېڭىلانغان ۋاقتى', // string | boolean
-
+		repo: 'https://github.com/IlhamTahir/mp-course-for-uyghur-language-starter',
+		repoLabel: 'ئامبار',
+		editLinkText: 'بەت خاتالىقىنى توغۇرلاشقا ياردەم قىلىڭ',
+		editLinks: true,
+		docsDir: 'docs',
+		smoothScroll: true,
+		logo: '/assets/img/logo.png',
+		nav: [
+			{ text: 'باش بەت', link: '/' },
+			{ text: 'مۇندەرىجە', link: '/0-catelog/' },
+		]
 	}
 }
